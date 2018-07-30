0b47ad08e296e5e39c463444e67cbf8e5ab2d251
Furtive Fox <furtivefox@gmail.com>
2018-07-29 18:06:25 -0700

    added constants to classes for cleaner importing

--- fchat/fchat.py
+++ fchat/fchat.py
@@ -1,6 +1,7 @@
 import json
 import requests
-import constants
+from .constants import flistEndpoints as E
+from .constants import requestsConstants as R
 
 
 class account:
@@ -9,13 +10,13 @@ class account:
         self.username = username
         self.userpass = userpass
         self.s = requests.Session()
-        self.s.headers.update({'User-Agent': userAgent})
+        self.s.headers.update({'User-Agent': R.userAgent})
 
     def init_auth(self):
         """Authenticates with f-list web api to get full acount info"""
         payload = {'account': self.username, 'password': self.userpass, 'new_character_list': "true"}
         try:
-            response = self.s.post(domain + getApiTicket, params=payload)
+            response = self.s.post(E.domain + E.getApiTicket, params=payload)
             response_dict = json.loads(response.text)
             self.acct_dict = response_dict
         except requests.exceptions.RequestException as e:
@@ -24,6 +25,6 @@ class account:
     def get_ticket(self):
         """Authenticates with f-list web api and requests minimal info"""
         payload = {'account': self.username, 'password': self.userpass, 'no_characters': 'true', 'no_friends': 'true', 'no_bookmarks': 'true'}
-        response = self.s.post(domain + getApiTicket, params=payload)
+        response = self.s.post(E.domain + E.getApiTicket, params=payload)
         response_dict = json.loads(response.text)
         self.ticket = response_dict['ticket']
