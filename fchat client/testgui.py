d2572451f93699b74e373eb3023b7a33849fc3e6
Jeff Paluck <jpaluck@voltagemultipliers.com>
2018-09-26 21:33:33 -0700

    added login button and check box.

--- fchat client/testgui.py
+++ fchat client/testgui.py
@@ -7,9 +7,10 @@ class loginwindow(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Login")
-        self.resizable(width=True, height=True)
+        # self.geometry("300x200")
+        self.resizable(width=False, height=False)
         loginframe = login_frame(self)
-        loginframe.grid()
+        loginframe.grid(padx=50, pady=50)
 
 
 class login_frame(tk.Frame):
@@ -20,12 +21,21 @@ class login_frame(tk.Frame):
 
         self.inputs = {}
         inputframe = tk.LabelFrame(self, text="Account Information")
-        self.inputs['Username'] = LI(inputframe, "Username: ", input_var=tk.StringVar())
-        self.inputs['Username'].grid(row=0, column=0)
+        self.inputs['Username'] = LI(inputframe, "Username: ", input_class=ttk.Entry, input_var=tk.StringVar())
+        self.inputs['Username'].grid(row=0, column=0, padx=15, pady=10)
 
-        self.inputs['Password'] = LI(inputframe, "Password: ", input_var=tk.StringVar(), input_args={'show': '*'})
-        self.inputs['Password'].grid(row=1, column=0)
-        inputframe.grid()
+        self.inputs['Password'] = LI(inputframe, "Password: ", input_class=ttk.Entry, input_var=tk.StringVar(), input_args={'show': '*'})
+        self.inputs['Password'].grid(row=1, column=0, padx=15)
+        inputframe.grid(columnspan=2)
+
+        self.saveloginbox = ttk.Checkbutton(self, text="Save Info")
+        self.saveloginbox.grid(row=2, column=0)
+
+        self.loginbutton = ttk.Button(self, text="Login", command=self.on_login)
+        self.loginbutton.grid(row=2, column=1, pady=25)
+
+    def on_login(self):
+        quit()
 
 
 if __name__ == '__main__':
--- fchat client/tksupport.py
+++ fchat client/tksupport.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 class LabelInput(tk.Frame):
     """A widget containing a label and input together."""
 
-    def __init__(self, parent, label='', input_class=ttk.Entry,
+    def __init__(self, parent, label='', input_class=None,
                  input_var=None, input_args=None, label_args=None,
                  **kwargs):
         super().__init__(parent, **kwargs)
